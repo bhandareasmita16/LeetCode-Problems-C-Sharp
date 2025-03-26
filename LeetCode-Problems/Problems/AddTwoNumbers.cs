26de4ec2dc1b959c062f73a8d235a4cdae69c8d4
bhandareasmita16 <bhandareasmita@gmail.com>
2025-03-26 12:52:00 +0800

    Add Two Numbers

---
+++ LeetCode-Problems/Problems/AddTwoNumbers.cs
@@ -0,0 +1,48 @@
+﻿using LeetCode_Problems.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Problems.Problems
+{
+    public class AddTwoNumbers
+    {
+        public ListNode AddTwoNumber(ListNode l1, ListNode l2)
+        {
+            ListNode dummy = new ListNode();
+            ListNode current = dummy;
+            int carry = 0;
+
+            while (l1 != null || l2 != null || carry != 0)
+            {
+                int val1 = (l1 != null) ? l1.val : 0;
+                int val2 = (l2 != null) ? l2.val : 0;
+
+                int sum = val1 + val2 + carry;
+                carry = sum / 10;
+                current.next = new ListNode(sum % 10);
+                current = current.next;
+
+                if (l1 != null) l1 = l1.next;
+                if (l2 != null) l2 = l2.next;
+            }
+
+            return dummy.next;
+        }
+        public void PrintList(ListNode head)
+        {
+            while (head != null)
+            {
+                Console.Write(head.val);
+                if (head.next != null)
+                    Console.Write(" -> ");
+                head = head.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
--- LeetCode-Problems/Program.cs
+++ LeetCode-Problems/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using LeetCode_Problems.Common;
+using LeetCode_Problems.Problems;
+
+AddTwoNumbers solution = new AddTwoNumbers();
+
+// Create two linked lists
+ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3))); // 342
+ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4))); // 465
+
+// Call AddTwoNumbers
+ListNode result = solution.AddTwoNumber(l1, l2);
+solution.PrintList(result);
